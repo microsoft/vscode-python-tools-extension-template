0e8c03601063c974a7c1555d355834a09e6e8f78
Christoph Waffler <waffler.christoph@gmail.com>
2023-05-16 14:44:43 +0200

    use resolver flag for correct dependency detection

--- noxfile.py
+++ noxfile.py
@@ -35,10 +35,11 @@ def _check_files(names: List[str]) -> None:
 
 
 def _update_pip_packages(session: nox.Session) -> None:
-    session.run("pip-compile", "--generate-hashes", "--upgrade", "./requirements.in")
+    session.run("pip-compile", "--generate-hashes", "--resolver=backtracking", "--upgrade", "./requirements.in")
     session.run(
         "pip-compile",
         "--generate-hashes",
+        "--resolver=backtracking",
         "--upgrade",
         "./src/test/python_tests/requirements.in",
     )
@@ -90,10 +91,11 @@ def _update_npm_packages(session: nox.Session) -> None:
 
 def _setup_template_environment(session: nox.Session) -> None:
     session.install("wheel", "pip-tools")
-    session.run("pip-compile", "--generate-hashes", "--upgrade", "./requirements.in")
+    session.run("pip-compile", "--generate-hashes", "--resolver=backtracking", "--upgrade", "./requirements.in")
     session.run(
         "pip-compile",
         "--generate-hashes",
+        "--resolver=backtracking",
         "--upgrade",
         "./src/test/python_tests/requirements.in",
     )
